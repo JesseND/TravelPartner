2a6b36d3bd7276792482044bea31af3ba4305354
Jesse Ndamutsa <jndamutsa14@gmail.com>
2019-06-17 12:19:40 -0500

    first commit

---
+++ TravelPartner/src/main/java/edu/mum/cs544/TravelPartnerApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs544;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPartnerApplication.class, args);
+	}
+
+}
