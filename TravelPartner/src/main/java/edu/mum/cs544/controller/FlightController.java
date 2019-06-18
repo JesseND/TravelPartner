9f2c59abefe615ffda48adf282d26c2c9876ff7b
david <mmtbz@qq.com>
2019-06-18 01:14:12 -0500

    flight integration

--- TravelPartner/src/main/java/edu/mum/cs544/controller/FlightController.java
+++ TravelPartner/src/main/java/edu/mum/cs544/controller/FlightController.java
@@ -14,7 +14,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/flights")
@@ -30,6 +35,12 @@ public class FlightController {
 
     @Autowired
     private AirplaneService airplaneService;
+
+    private DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
+            Locale.US);
+
+    private  DateFormat tf = DateFormat.getTimeInstance(DateFormat.SHORT,
+            Locale.US);
 
     @GetMapping()
     public String allAirports(Model model){
@@ -55,9 +66,79 @@ public class FlightController {
         return "flight/flightDetail";
     }
 
+//    @PostMapping()
+//    public String add(Flight flight){
+//        flightService.add(flight);
+//        return "redirect:/flights";
+//    }
+
     @PostMapping()
-    public String add(Flight flight){
-        flightService.add(flight);
+    public String add(String flightnr, String departureDate, String departureTime,
+                      String arrivalDate, String arrivalTime, String airline,
+                      String airplane, String origin, String destination) throws ParseException {
+
+//        System.out.println(flightnr +"\n"+
+//                departureDate +"\n"+
+//                departureTime +"\n"+
+//                arrivalDate +"\n"+
+//                arrivalTime +"\n"+
+//                airline +"\n"+
+//                airplane +"\n"+
+//                origin +"\n"+
+//                destination);
+//        SimpleDateFormat  spdf = new SimpleDateFormat("yyyy-MM-dd");
+//        SimpleDateFormat  sptf = new SimpleDateFormat("HH:mm");
+//
+//        Date depDate = spdf.parse(departureDate);
+//        Date depTime = sptf.parse(departureTime);
+//
+//        Date arrDate = spdf.parse(arrivalDate);
+//        Date arrTime = sptf.parse(arrivalTime);
+//
+//        Airline airline1 = airlineService.getOne(Long.parseLong(airline));
+//        airlineService.add(airline1);
+//        Airplane airplane1 = airplaneService.getOne(Long.parseLong(airplane));
+//        airplaneService.add(airplane1);
+//        Airport originAirport = airportService.getById(Long.parseLong(origin));
+//        airportService.add(originAirport);
+//        Airport destAirport = airportService.getById(Long.parseLong(destination));
+//        airportService.add(destAirport);
+//
+//        Flight flight = new Flight();
+//
+//        // set things
+//        flight.setAirline(airline1);
+//        flight.setAirplane(airplane1);
+//        flight.setFlightnr(flightnr);
+//        flight.setDestination(destAirport);
+//        flight.setOrigin(originAirport);
+//        flight.setDepartureDate(depDate);
+//        flight.setDepartureTime(depTime);
+//        flight.setArrivalDate(arrDate);
+//        flight.setArrivalTime(arrTime);
+//
+//        System.out.println("new flight: "+flight);
+//        // save other entities before
+//
+
+        // flightService.add(flight);
+
+        String updateQuery =
+                "INSERT INTO `ticketmicro`.`flight`" +
+                        "(`arrival_date`, `arrival_time`, " +
+                        "`departure_date`, `departure_time`, `flightnr`," +
+                        " `airline_id`, `airplane_id`, `destination_id`, `origin_id`) " +
+                        "VALUES ('"+ arrivalDate+ "', '"+
+                        arrivalTime+ "', '"+
+                        departureDate+"', '"+
+                        departureTime+"', '"+
+                        flightnr+"', "+
+                        Long.parseLong(airline)+" , "+
+                        Long.parseLong(airplane)+" , "+
+                        Long.parseLong(destination)+", "+
+                        Long.parseLong(origin)+ ")";
+
+        flightService.addByQuery(updateQuery);
         return "redirect:/flights";
     }
 
