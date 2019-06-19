66d526dc24a597e663dcae2e75f6f4f7e27edd30
david <mmtbz@qq.com>
2019-06-18 20:14:54 -0500

    added update for all use cases

---
+++ TravelPartner/src/main/java/edu/mum/cs544/controller/basicErrorController.java
@@ -0,0 +1,14 @@
+//package edu.mum.cs544.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class basicErrorController {
+//
+//    @RequestMapping(value = "/error")
+//    public String error(){
+//        return "404 not found";
+//    }
+//
+//}
